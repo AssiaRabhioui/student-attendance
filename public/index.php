370bb1d475941748921db4f93e74442a474c562d
AssiaRabhioui <assia.rabhioui@student.hepl.be>
2026-02-18 16:25:12 +0100

    Fix association and code

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
 
 const PUBLIC_PASS = __DIR__;
-const APP_PASS = PUBLIC_PASS.'/..';
-const  VIEWS_PASS = APP_PASS.'/views';
+const APP_PASS = PUBLIC_PASS . '/..';
+const  VIEWS_PASS = APP_PASS . '/views';
+const  VENDOR_PASS = APP_PASS . '/vendor';
+require VENDOR_PASS . '/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(APP_PASS);
+$dotenv->load();
+
 
 include '../db/queries.php';
 
@@ -13,17 +18,17 @@ switch ($_SERVER['REQUEST_URI']) {
     case '':
     case '/':
         $title = 'Page d’accueil';
-        include VIEWS_PASS.'/home.php';
+        include VIEWS_PASS . '/home.php';
         break;
     case '/presences':
         $title = 'Prendre les présences';
-        include VIEWS_PASS.'/attendances/index.php';
+        include VIEWS_PASS . '/attendances/index.php';
         break;
     case '/etudiants':
         $title = 'Tous les étudiants';
-        include VIEWS_PASS.'/students/index.php';
+        include VIEWS_PASS . '/students/index.php';
         break;
     default:
         $title = '404';
-        include VIEWS_PASS.'/404.php';
+        include VIEWS_PASS . '/404.php';
 }
